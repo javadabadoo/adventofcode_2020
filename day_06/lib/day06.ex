ff470f0f5534162d0b87fa6edd8eb6289d7f3fe2
javadabadoo <gerardo.agalicia@gmail.com>
2021-03-15 22:50:27 -0600

    Day 06 - Part 2

--- day_06/lib/day06.ex
+++ day_06/lib/day06.ex
@@ -3,8 +3,17 @@ defmodule Day06 do
     unique_answer_sum = "./test/input.txt"
       |> Util.Reader.read_from_file()
       |> Util.Helper.clean_lines([])
-      |> Util.Helper.count_unique_answers(0)
+      |> Util.Helper.count_unique_answers()
 
     "Sum of all grups unique anwers is #{unique_answer_sum}"
+  end
+
+  def second do
+    unique_answer_sum = "./test/input.txt"
+      |> Util.Reader.read_from_file()
+      |> Util.Helper.extract_common_answer([])
+      |> Util.Helper.count_unique_answers()
+
+    "Sum of all commons answers is #{inspect unique_answer_sum}"
   end
 end
--- day_06/lib/util/helper.ex
+++ day_06/lib/util/helper.ex
@@ -10,9 +10,40 @@ defmodule Util.Helper do
 
   def clean_lines([], acc), do: Enum.reverse(acc)
 
-  def count_unique_answers([h|t], counter) do
-    count_unique_answers(t, counter + Enum.count(h))
+  def count_unique_answers(answers) do
+    answers
+      |> List.flatten()
+      |> Enum.count()
   end
 
-  def count_unique_answers([], counter), do: counter
+  def extract_common_answer([group_responses|t], common_answers) do
+    extract_common_answer(t, [get_common_answers(Enum.to_list(?a..?z), group_responses, [])| common_answers])
+  end
+
+  def extract_common_answer([], common_answers), do: common_answers
+
+  def get_common_answers([current_letter|t], group_responses, common_answers) do
+    if has_all_yes_anwers(current_letter, group_responses) do
+      get_common_answers(t, group_responses, [<< current_letter :: utf8>> | common_answers])
+    else
+      get_common_answers(t, group_responses, common_answers)
+    end
+  end
+
+  def get_common_answers([], _, common_answers), do: common_answers
+
+  def has_all_yes_anwers(letter, group_responses) do
+    {total_responses, cleaned_responses} = join_responses(group_responses)
+    Regex.scan(~r/#{<<letter :: utf8>>}/, cleaned_responses)
+    |> Enum.count() == total_responses
+  end
+
+  def join_responses(responses) do
+    {
+      Regex.scan(~r/\n./, responses) |> Enum.count() |> Kernel.+(1),
+      String.replace(responses, "\n", "")
+    }
+  end
+
+  ?a..?z
 end
